75cb74f5d3bdd39e8ae123d508fe0d1bb865b542
VaticanMemes <popeharryviii@gmail.com>
2024-12-01 21:41:05 +1100

    get_started page

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+
 @app.route('/home')
 def home():
     return render_template('index.html')
+
+@app.route('/get_started')
+def get_started():
+    return render_template('get_started.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
