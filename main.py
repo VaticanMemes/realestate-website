8be7c8c1ed0724bd38ffa19c91fe905592d7e258
VaticanMemes <popeharryviii@gmail.com>
2024-12-02 17:25:34 +1100

    few changes

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+from wtforms.validators import InputRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
 
 @app.route('/')
-
 @app.route('/home')
 def home():
     return render_template('index.html')
 
-@app.route('/get_started')
+@app.route('/get_started', methods=['GET', 'POST'])
 def get_started():
-    return render_template('get_started.html')
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+        return "File has been uploaded."
+    return render_template('get_started.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
